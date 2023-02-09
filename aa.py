2c4d5e0a1ddaea97bfbda0d5c8d9ca248dce79b5
phithuan <turnthuan1@gmail.com>
2023-02-09 22:59:57 +0700

    new

--- aa.py
+++ aa.py
@@ -1,19 +1,13 @@
-import datetime
-print('nhập ')
-print("Mời bạn vui lòng nhập ngày tháng năm sinh để tính tuổi")
-birth_day = int(input("Ngày sinh:"))
-birth_month = int(input("Tháng sinh:"))
-birth_year = int(input("Năm sinh:"))
+def is_prime():
+    count = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            count += 1
+    if count == 2:
+        return True
+    return False
 
-current_year = datetime.date.today().year
-current_month = datetime.date.today().month
-current_day = datetime.date.today().day
 
-age_year = current_year - birth_year
-age_month = abs(current_month-birth_month)
-age_day = abs(current_day-birth_day)
-
-print("### Tuổi của bạn chính xác là:### \n", age_year, " tuổi ", age_month, " tháng và ", age_day, " ngày")
-
-print('lần đầu test sourcetree')
-#test
+n = int(input())
+print(is_prime(n))
+is_prime()
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print('5'*5)
+n=int(input('nhập số: '))
+for i in range(n):
+    for j in range(i+1):
+        print(n,end=" ")
+    print()
+    
